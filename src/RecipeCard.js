4cff870c38864269972ea04394adc89c256430bf
Neelabh Kashyap <neelabh.kashyap@bigdatapump.com>
2017-11-27 18:38:21 +0200

    created mockup of finished recipe card

--- src/RecipeCard.js
+++ src/RecipeCard.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './RecipeCard.css';
+import recipes from './recipes.json';
+import similarDrinks from './similar-drinks.json';
 
 class RecipeCard extends Component {
     constructor(props) {
         super(props);
 
         this.state = {displayStyle: 'none'};
+        this.state.contentItem = this.props.contentItem;
     }
 
     toggleRecipe() {
@@ -14,15 +17,40 @@ class RecipeCard extends Component {
         else
             this.setState({displayStyle: 'none'});;
     }
-    
+
+    getSimilarDrinks(recipeLink, ordinal) {
+        const getDrinkName = link => {
+            return(recipes.filter(x=>x.recipeLink === link)[0].name);
+        };
+
+        const filtered = similarDrinks.filter( x => x.recipeLink === recipeLink);
+        if (ordinal === 1)
+            return(getDrinkName(filtered[0].similarDrink1));
+        else
+        return(getDrinkName(filtered[0].similarDrink2));
+    }
+
     render() {
         return (
-        <div className='card' onClick={this.toggleRecipe.bind(this)}>
-        <h4 className='card-title'>{this.props.contentItem.name}</h4>
+        <div className='card'>
+        <h4 className='card-title' onClick={this.toggleRecipe.bind(this)}>{this.state.contentItem.name}</h4>
         <div className='card-content' style={{display: this.state.displayStyle}}>
-        <p>{this.props.contentItem.ingredients}</p>
-        <p>{this.props.contentItem.instructions}</p>
+        <p>{this.state.contentItem.ingredients}</p>
+        <p>{this.state.contentItem.instructions}</p>
+        <hr/>
+        <h4>Similar drinks</h4>
+        <div className="similar">
+        <button className="btn similar-button">{this.getSimilarDrinks(this.state.contentItem.recipeLink,1)}</button>
+        <button className="btn similar-button">{this.getSimilarDrinks(this.state.contentItem.recipeLink,2)}</button>
         </div>
+        <hr/>
+        <h4>Rating</h4>
+        <div style={{display: "inline"}}>
+        <button className="btn"><i className="icon icon-thumbs-up"/></button>
+        <button className="btn"><i className="icon icon-thumbs-down"/></button>
+        </div>
+        </div>
+        
         </div>
         );
     }
