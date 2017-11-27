ea2788bd0f6cf84e45514340aa71953be68b08de
Neelabh Kashyap <neelabh.kashyap@bigdatapump.com>
2017-11-27 19:03:22 +0200

    toggles content on similar drink button press

--- src/RecipeCard.js
+++ src/RecipeCard.js
@@ -9,9 +9,12 @@ class RecipeCard extends Component {
 
         this.state = {displayStyle: 'none'};
         this.state.contentItem = this.props.contentItem;
+        this.state.displayContent = this.props.contentItem;
+   
     }
 
     toggleRecipe() {
+        this.setState({displayContent: this.state.contentItem});
         if (this.state.displayStyle === 'none')
             this.setState({displayStyle: null});
         else
@@ -30,24 +33,44 @@ class RecipeCard extends Component {
         return(getDrinkName(filtered[0].similarDrink2));
     }
 
+    changeContent(ordinal){
+        const getSimilarDrinkContent = (recipeLink, ordinal) => {
+            const getDrinkContent = link => {
+                return(recipes.filter(x=>x.recipeLink === link)[0]);
+            };
+    
+            const filtered = similarDrinks.filter( x => x.recipeLink === recipeLink);
+            if (ordinal === 1)
+                return(getDrinkContent(filtered[0].similarDrink1));
+            else
+            return(getDrinkContent(filtered[0].similarDrink2));
+        }
+
+        const recipeLink = this.state.contentItem.recipeLink;
+
+        this.setState({displayContent: getSimilarDrinkContent(recipeLink, ordinal)});
+    }
+
+    
+
     render() {
         return (
         <div className='card'>
-        <h4 className='card-title' onClick={this.toggleRecipe.bind(this)}>{this.state.contentItem.name}</h4>
+        <h4 className='card-title' onClick={this.toggleRecipe.bind(this)}>{this.state.displayContent.name}</h4>
         <div className='card-content' style={{display: this.state.displayStyle}}>
-        <p>{this.state.contentItem.ingredients}</p>
-        <p>{this.state.contentItem.instructions}</p>
+        <p>{this.state.displayContent.ingredients}</p>
+        <p>{this.state.displayContent.instructions}</p>
         <hr/>
         <h4>Similar drinks</h4>
         <div className="similar">
-        <button className="btn similar-button">{this.getSimilarDrinks(this.state.contentItem.recipeLink,1)}</button>
-        <button className="btn similar-button">{this.getSimilarDrinks(this.state.contentItem.recipeLink,2)}</button>
+        <button className="btn similar-button" onClick={() => this.changeContent(1)}>{this.getSimilarDrinks(this.state.contentItem.recipeLink,1)}</button>
+        <button className="btn similar-button" onClick={() => this.changeContent(2)}>{this.getSimilarDrinks(this.state.contentItem.recipeLink,2)}</button>
         </div>
         <hr/>
         <h4>Rating</h4>
         <div style={{display: "inline"}}>
-        <button className="btn"><i className="icon icon-thumbs-up"/></button>
-        <button className="btn"><i className="icon icon-thumbs-down"/></button>
+        <button className="btn similar-button"><i className="icon icon-thumbs-up"/></button>
+        <button className="btn similar-button"><i className="icon icon-thumbs-down"/></button>
         </div>
         </div>
         
